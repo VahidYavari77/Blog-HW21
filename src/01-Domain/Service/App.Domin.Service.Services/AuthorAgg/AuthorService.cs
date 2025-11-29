e0776a1781e0cef38ff6eb4b4ef0a2d40786c195
VahidYavari77 <atoo77vahid@gmail.com>
2025-11-29 17:10:09 +0330

    Refactor services and app services to inherit from interfaces

--- src/01-Domain/Service/App.Domin.Service.Services/AuthorAgg/AuthorService.cs
+++ src/01-Domain/Service/App.Domin.Service.Services/AuthorAgg/AuthorService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using App.Domain.Core.AuthorAgg.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace App.Domin.Service.Services.AuthorAgg
 {
-   public class AuthorService
+   public class AuthorService: IAuthorService
     {
     }
 }
--- src/01-Domain/Service/App.Domin.Service.Services/CategoryAgg/CategoryService.cs
+++ src/01-Domain/Service/App.Domin.Service.Services/CategoryAgg/CategoryService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using App.Domain.Core.AuthorAgg.Contracts;
+using App.Domain.Core.CategoryAgg.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace App.Domin.Service.Services.CategoryAgg
 {
-  public  class CategoryService
+  public  class CategoryService : ICategoryService
     {
     }
 }
