66cfbe7a9d7fd6c1d8fc21f10f8fe816c8124101
VahidYavari77 <atoo77vahid@gmail.com>
2025-11-29 16:53:49 +0330

    Create interfaces and services (AppService, Repository, Service)

---
+++ src/01-Domain/Service/App.Domin.Service.AppServices/CommentAgg/CommentAppService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domin.Service.AppServices.CommentAgg
+{
+   public class CommentAppService
+    {
+    }
+}
